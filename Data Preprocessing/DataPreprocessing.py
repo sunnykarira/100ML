f77f54a379a8a49b1100d9c6a2d61fc9440cfb53
Sunny Karira <sunny@sendinblue.com>
2018-07-06 23:13:53 +0530

    Importing Libraries

---
+++ Data Preprocessing/DataPreprocessing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  6 23:10:31 2018
+
+@author: sunnykarira
+"""
+
+# Data Preprocessing Templates
+
+# Importing the libraries
+import numpy as np # For mathematical calculation
+import matplotlib.pyplot as plt # For plotting charts
+import pandas as pd  # To import and manage datasets
