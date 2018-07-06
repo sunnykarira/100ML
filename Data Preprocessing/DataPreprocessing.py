01173a2643c96ad284e18049f6e66d7ee025e1fe
Sunny Karira <sunny@sendinblue.com>
2018-07-06 23:38:16 +0530

    Sklearn filling missing data by strategy

--- Data Preprocessing/DataPreprocessing.py
+++ Data Preprocessing/DataPreprocessing.py
@@ -41,4 +41,31 @@ y = dataset.iloc[:, 3].values
 
 '''array(['No', 'Yes', 'No', 'No', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes'])'''
 
+# Preparing the data
+
+# Take care of missing data
+
+from sklearn.preprocessing import Imputer
+# cmd + I to inspect (MAC)
+imputer = Imputer(missing_values = 'NaN', strategy = "mean", axis = 0)
+# Fitting the imputer to matrix 
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1: 3] = imputer.transform(X[:, 1:3])
+
+# Took mean of columns and replaced nan values
+'''([['France', 44.0, 72000.0],
+       ['Spain', 27.0, 48000.0],
+       ['Germany', 30.0, 54000.0],
+       ['Spain', 38.0, 61000.0],
+       ['Germany', 40.0, 63777.77777777778],
+       ['France', 35.0, 58000.0],
+       ['Spain', 38.77777777777778, 52000.0],
+       ['France', 48.0, 79000.0],
+       ['Germany', 50.0, 83000.0],
+       ['France', 37.0, 67000.0]])'''
+
+
+
+
+
 
